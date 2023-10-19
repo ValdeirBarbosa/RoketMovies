0a3afa1cb6809984f458a6ca2a043f3e46299d82
ValdeirBarbosa <valdeir.barbosa@outlook.com>
2023-10-18 21:44:20 -0300

    ajuste em cards movie

--- src/Components/MovieCard/styles.js
+++ src/Components/MovieCard/styles.js
@@ -2,17 +2,37 @@ import styled from "styled-components"
 
 
 export const Container = styled.div`
-background-color: blue;
-width:100%;
-height: 200px;
-border-radius: 18px;
-padding: 15px;
+background-color: ${({ theme }) => theme.COLORS.PINK_LUCID};
+/* border:1px solid ${({ theme }) => theme.COLORS.PINK}; */
+display: flex;
+padding: 32px;
+flex-direction: column;
+align-items: flex-start;
+gap: 15px;
+border-radius:18px;
 
+h1{
+   color:${({ theme }) => theme.COLORS.CARD_FONT_COLOR_TITLE};
+   text-transform: capitalize;
+}
 
 .description-movie{
-  text-align:justify;
-  max-height: 100px;
-  overflow-y:hidden
+  text-align:justify; 
+  overflow-y:scroll;
+  font-size: 16px;
+  color:${({ theme }) => theme.COLORS.CARD_FONT_COLOR_DESCRIPTION};
+  display: -webkit-box;
+-webkit-box-orient: vertical;
+-webkit-line-clamp: 2;
+align-self: stretch;
+overflow: hidden;
+color: var(--Gray, #999591);
+text-overflow: ellipsis;
+font-family: Roboto;
+font-size: 16px;
+font-style: normal;
+font-weight: 400;
+line-height: normal;
 }
 
 `
